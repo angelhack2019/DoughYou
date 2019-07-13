308d58b01bb12c54df09aad1f7245fcdfb4e899e
Khaoula Bahloul <khaoula@uw.edu>
2019-07-13 14:04:11 -0700

    Initial commit

---
+++ DoughYou/DoughYou.Android/obj/Debug/90/android/src/md5e47b6f6b3ed9030408d38cb25e1a0a07/SplashActivity.java
@@ -0,0 +1,56 @@
+package md5e47b6f6b3ed9030408d38cb25e1a0a07;
+
+
+public class SplashActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;Landroid/os/PersistableBundle;)V:GetOnCreate_Landroid_os_Bundle_Landroid_os_PersistableBundle_Handler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"";
+		mono.android.Runtime.register ("DoughYou.Droid.SplashActivity, DoughYou.Android", SplashActivity.class, __md_methods);
+	}
+
+
+	public SplashActivity ()
+	{
+		super ();
+		if (getClass () == SplashActivity.class)
+			mono.android.TypeManager.Activate ("DoughYou.Droid.SplashActivity, DoughYou.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0, android.os.PersistableBundle p1)
+	{
+		n_onCreate (p0, p1);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0, android.os.PersistableBundle p1);
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
